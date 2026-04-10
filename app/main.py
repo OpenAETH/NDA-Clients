fd085d0370950c56dacca32346aa9d2bdc9f8d91
AETHERYON Systems <131323974+OpenAETH@users.noreply.github.com>
2026-04-10 02:18:11 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -66,4 +66,4 @@ else:
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(main, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
