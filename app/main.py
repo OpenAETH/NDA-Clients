0f79809ea17811537e873edf5d56bd078c14d2d9
AETHERYON Systems <131323974+OpenAETH@users.noreply.github.com>
2026-04-10 02:41:23 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from contextlib import asynccontextmanager
 from pathlib import Path
-
+import os
 from app.core.database import connect_db, close_db
 from app.routers import products, engagements, payments
 
